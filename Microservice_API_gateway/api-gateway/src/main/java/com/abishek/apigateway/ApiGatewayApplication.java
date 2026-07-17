01b40e4dc1cf74247c243865676c1792209cf9da
Abishek M <abishekm290406@gmail.com>
2026-07-17 22:50:05 +0530

    accountservice

---
+++ Microservice_API_gateway/api-gateway/src/main/java/com/abishek/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,5 @@
+package api-gateway.src.main.java.com.abishek.apigateway;
+
+public class ApiGatewayApplication {
+    
+}
---
+++ Microservice_API_gateway/loan-service/src/main/java/com/abishek/loanservice/LoanServiceApplication.java
@@ -0,0 +1,5 @@
+package loan-service.main.java.com.abishek.loanservice;
+
+public class LoanServiceApplication {
+    
+}
