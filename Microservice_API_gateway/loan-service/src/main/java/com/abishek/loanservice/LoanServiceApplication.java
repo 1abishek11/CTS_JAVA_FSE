d86613f818e919f132d9289be0c621421d9ac115
Abishek M <abishekm290406@gmail.com>
2026-07-26 15:05:17 +0530

    n

--- Microservice_API_gateway/loan-service/src/main/java/com/abishek/loanservice/LoanServiceApplication.java
+++ Microservice_API_gateway/loan-service/src/main/java/com/abishek/loanservice/LoanServiceApplication.java
@@ -1,5 +1,13 @@
-package loan-service.main.java.com.abishek.loanservice;
+package com.abishek.loanservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class LoanServiceApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanServiceApplication.class, args);
+    }
+
 }
